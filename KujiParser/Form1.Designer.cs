2529c25d787cb3baca7b3f3da6e9ae67662fd3ee
RJ <crongjie@hotmail.com>
2017-01-11 00:21:39 +0900

    completed

--- KujiParser/Form1.Designer.cs
+++ KujiParser/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.tb_output = new System.Windows.Forms.TextBox();
+            this.btn_process = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tb_file = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(12, 12);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(161, 31);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // tb_output
+            // 
+            this.tb_output.Location = new System.Drawing.Point(16, 51);
+            this.tb_output.MaxLength = 3276700;
+            this.tb_output.Multiline = true;
+            this.tb_output.Name = "tb_output";
+            this.tb_output.Size = new System.Drawing.Size(1028, 430);
+            this.tb_output.TabIndex = 1;
+            // 
+            // btn_process
+            // 
+            this.btn_process.Location = new System.Drawing.Point(308, 501);
+            this.btn_process.Name = "btn_process";
+            this.btn_process.Size = new System.Drawing.Size(118, 27);
+            this.btn_process.TabIndex = 2;
+            this.btn_process.Text = "Process";
+            this.btn_process.UseVisualStyleBackColor = true;
+            this.btn_process.Click += new System.EventHandler(this.btn_process_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(506, 501);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(118, 27);
+            this.btn_save.TabIndex = 3;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "kuji100";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "json";
+            this.saveFileDialog1.FileName = "output";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // tb_file
+            // 
+            this.tb_file.Location = new System.Drawing.Point(192, 21);
+            this.tb_file.Name = "tb_file";
+            this.tb_file.ReadOnly = true;
+            this.tb_file.Size = new System.Drawing.Size(556, 19);
+            this.tb_file.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 540);
+            this.Controls.Add(this.tb_file);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_process);
+            this.Controls.Add(this.tb_output);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.TextBox tb_output;
+        private System.Windows.Forms.Button btn_process;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox tb_file;
     }
 }
 
--- KujiParser/Form1.cs
+++ KujiParser/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 
 namespace KujiParser
 {
@@ -15,6 +17,128 @@ namespace KujiParser
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string file_name = "";
+        //第一簽
+        //簽詞
+        //概述
+        //詩曰
+        //解曰
+        //釋義
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file_name = openFileDialog1.FileName;
+            }
+            else
+            {
+                file_name = "";
+            }
+            tb_file.Text = file_name;
+        }
+        private JArray list = new JArray();
+        private void btn_process_Click(object sender, EventArgs e)
+        {
+            if (file_name.Length > 0)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                string line = "";
+                int i = 0;
+                list = new JArray();
+                JArray interpretation = new JArray();
+                JObject jo = new JObject();
+                String nextKey = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.IndexOf('[') >= 0)
+                    {
+                        tb_output.Text += "\r\n" + line.Substring(line.IndexOf('[')+1, line.IndexOf(']') - 1);
+
+
+                        if (line.IndexOf("[第") >= 0)
+                        {
+                            list.Add(jo);
+                            jo = new JObject();
+                            jo["num"] = line.Substring(line.IndexOf('[') + 1, line.IndexOf(']') - 1);
+                            nextKey = "title";
+                        }
+                        else if (line.IndexOf("[簽詞]") >= 0)
+                        {
+                            nextKey = "text";
+                        }
+                        else if (line.IndexOf("[概述]") >= 0)
+                        {
+                            nextKey = "overview";
+                        }
+                        else if (line.IndexOf("[詩曰]") >= 0)
+                        {
+                            nextKey = "poetry";
+                        }
+                        else if (line.IndexOf("[解曰]") >= 0)
+                        {
+                            nextKey = "explanation";
+                        }
+                        else if (line.IndexOf("[釋義]") >= 0)
+                        {
+                            nextKey = "interpretation";
+                            interpretation = new JArray();
+                        }
+                    }
+                    else
+                    {
+                        if (nextKey == "interpretation")
+                        {
+                            if (line.TrimEnd().Length > 0)
+                            {
+                                if (line.IndexOf('：') >= 0)
+                                {
+                                    int sep = line.IndexOf('：');
+                                    JObject intep_item = new JObject();
+                                    intep_item["key"] = line.Substring(0, sep);
+                                    intep_item["value"] = line.Substring(sep + 1, line.Length - sep -1);
+                                    interpretation.Add(intep_item);
+                                }
+                            }
+    
+                            jo["interpretation"] = interpretation;
+                        }else if (nextKey.Length > 0 )
+                        {
+                            jo[nextKey] = line;
+                        }
+                    }
+
+                }
+                list.Add(jo);
+
+
+                sr.Close();
+
+                tb_output.Text = list.ToString();
+            }
+            else
+            {
+                MessageBox.Show("File is not opened.");
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new
+                   System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.UTF8);
+                sw.Write(list.ToString());
+                sw.Close();
+            }
         }
     }
 }
