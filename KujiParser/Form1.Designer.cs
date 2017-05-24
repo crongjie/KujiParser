9404baf38698c0c71a3a52b942a7c59cdc93128d
RJ <crongjie@hotmail.com>
2017-05-25 03:54:04 +0900

    added processes

--- KujiParser/Form1.Designer.cs
+++ KujiParser/Form1.Designer.cs
@@ -35,6 +35,15 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.tb_file = new System.Windows.Forms.TextBox();
+            this.btn_process2 = new System.Windows.Forms.Button();
+            this.tb_file2 = new System.Windows.Forms.TextBox();
+            this.btn_open2 = new System.Windows.Forms.Button();
+            this.btn_process3 = new System.Windows.Forms.Button();
+            this.btn_process4 = new System.Windows.Forms.Button();
+            this.cb_csv = new System.Windows.Forms.CheckBox();
+            this.btn_process_save = new System.Windows.Forms.Button();
+            this.btn_process5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_open
@@ -49,17 +58,17 @@
             // 
             // tb_output
             // 
-            this.tb_output.Location = new System.Drawing.Point(16, 51);
+            this.tb_output.Location = new System.Drawing.Point(16, 85);
             this.tb_output.MaxLength = 3276700;
             this.tb_output.Multiline = true;
             this.tb_output.Name = "tb_output";
             this.tb_output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tb_output.Size = new System.Drawing.Size(1028, 430);
+            this.tb_output.Size = new System.Drawing.Size(1028, 396);
             this.tb_output.TabIndex = 1;
             // 
             // btn_process
             // 
-            this.btn_process.Location = new System.Drawing.Point(308, 501);
+            this.btn_process.Location = new System.Drawing.Point(140, 501);
             this.btn_process.Name = "btn_process";
             this.btn_process.Size = new System.Drawing.Size(118, 27);
             this.btn_process.TabIndex = 2;
@@ -69,7 +78,7 @@
             // 
             // btn_save
             // 
-            this.btn_save.Location = new System.Drawing.Point(506, 501);
+            this.btn_save.Location = new System.Drawing.Point(926, 501);
             this.btn_save.Name = "btn_save";
             this.btn_save.Size = new System.Drawing.Size(118, 27);
             this.btn_save.TabIndex = 3;
@@ -96,11 +105,108 @@
             this.tb_file.Size = new System.Drawing.Size(556, 19);
             this.tb_file.TabIndex = 4;
             // 
+            // btn_process2
+            // 
+            this.btn_process2.Location = new System.Drawing.Point(16, 501);
+            this.btn_process2.Name = "btn_process2";
+            this.btn_process2.Size = new System.Drawing.Size(118, 27);
+            this.btn_process2.TabIndex = 5;
+            this.btn_process2.Text = "Process 2";
+            this.btn_process2.UseVisualStyleBackColor = true;
+            this.btn_process2.Click += new System.EventHandler(this.btn_process2_Click);
+            // 
+            // tb_file2
+            // 
+            this.tb_file2.Location = new System.Drawing.Point(192, 57);
+            this.tb_file2.Name = "tb_file2";
+            this.tb_file2.ReadOnly = true;
+            this.tb_file2.Size = new System.Drawing.Size(556, 19);
+            this.tb_file2.TabIndex = 7;
+            // 
+            // btn_open2
+            // 
+            this.btn_open2.Location = new System.Drawing.Point(12, 48);
+            this.btn_open2.Name = "btn_open2";
+            this.btn_open2.Size = new System.Drawing.Size(161, 31);
+            this.btn_open2.TabIndex = 6;
+            this.btn_open2.Text = "Open";
+            this.btn_open2.UseVisualStyleBackColor = true;
+            this.btn_open2.Click += new System.EventHandler(this.btn_open2_Click);
+            // 
+            // btn_process3
+            // 
+            this.btn_process3.Location = new System.Drawing.Point(264, 501);
+            this.btn_process3.Name = "btn_process3";
+            this.btn_process3.Size = new System.Drawing.Size(125, 27);
+            this.btn_process3.TabIndex = 8;
+            this.btn_process3.Text = "2text(type1)";
+            this.btn_process3.UseVisualStyleBackColor = true;
+            this.btn_process3.Click += new System.EventHandler(this.btn_process3_Click);
+            // 
+            // btn_process4
+            // 
+            this.btn_process4.Location = new System.Drawing.Point(395, 501);
+            this.btn_process4.Name = "btn_process4";
+            this.btn_process4.Size = new System.Drawing.Size(125, 27);
+            this.btn_process4.TabIndex = 9;
+            this.btn_process4.Text = "2text(hydcd-word)";
+            this.btn_process4.UseVisualStyleBackColor = true;
+            this.btn_process4.Click += new System.EventHandler(this.btn_process4_Click);
+            // 
+            // cb_csv
+            // 
+            this.cb_csv.AutoSize = true;
+            this.cb_csv.Location = new System.Drawing.Point(939, 59);
+            this.cb_csv.Name = "cb_csv";
+            this.cb_csv.Size = new System.Drawing.Size(47, 16);
+            this.cb_csv.TabIndex = 10;
+            this.cb_csv.Text = "CSV";
+            this.cb_csv.UseVisualStyleBackColor = true;
+            // 
+            // btn_process_save
+            // 
+            this.btn_process_save.Location = new System.Drawing.Point(789, 501);
+            this.btn_process_save.Name = "btn_process_save";
+            this.btn_process_save.Size = new System.Drawing.Size(118, 27);
+            this.btn_process_save.TabIndex = 11;
+            this.btn_process_save.Text = "Process Save";
+            this.btn_process_save.UseVisualStyleBackColor = true;
+            this.btn_process_save.Click += new System.EventHandler(this.btn_process_save_Click);
+            // 
+            // btn_process5
+            // 
+            this.btn_process5.Location = new System.Drawing.Point(526, 501);
+            this.btn_process5.Name = "btn_process5";
+            this.btn_process5.Size = new System.Drawing.Size(125, 27);
+            this.btn_process5.TabIndex = 12;
+            this.btn_process5.Text = "2text(sample)";
+            this.btn_process5.UseVisualStyleBackColor = true;
+            this.btn_process5.Click += new System.EventHandler(this.btn_process5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 27);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "2text(comma)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 540);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_process5);
+            this.Controls.Add(this.btn_process_save);
+            this.Controls.Add(this.cb_csv);
+            this.Controls.Add(this.btn_process4);
+            this.Controls.Add(this.btn_process3);
+            this.Controls.Add(this.tb_file2);
+            this.Controls.Add(this.btn_open2);
+            this.Controls.Add(this.btn_process2);
             this.Controls.Add(this.tb_file);
             this.Controls.Add(this.btn_save);
             this.Controls.Add(this.btn_process);
@@ -108,6 +214,7 @@
             this.Controls.Add(this.btn_open);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +229,15 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.TextBox tb_file;
+        private System.Windows.Forms.Button btn_process2;
+        private System.Windows.Forms.TextBox tb_file2;
+        private System.Windows.Forms.Button btn_open2;
+        private System.Windows.Forms.Button btn_process3;
+        private System.Windows.Forms.Button btn_process4;
+        private System.Windows.Forms.CheckBox cb_csv;
+        private System.Windows.Forms.Button btn_process_save;
+        private System.Windows.Forms.Button btn_process5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- KujiParser/Form1.cs
+++ KujiParser/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace KujiParser
 {
@@ -20,6 +21,7 @@ namespace KujiParser
         }
 
         private string file_name = "";
+        private string file_name2 = "";
         //第一簽
         //簽詞
         //概述
@@ -39,6 +41,8 @@ namespace KujiParser
             tb_file.Text = file_name;
         }
         private JArray list = new JArray();
+        private String list_csv = "";
+
         private void btn_process_Click(object sender, EventArgs e)
         {
             if (file_name.Length > 0)
@@ -136,7 +140,322 @@ namespace KujiParser
             {
                 System.IO.StreamWriter sw = new
                    System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.UTF8);
-                sw.Write(list.ToString());
+                if (cb_csv.Checked) sw.Write(list_csv);
+                else sw.Write(list.ToString());
+
+                sw.Close();
+            }
+        }
+
+
+        private void btn_process2_Click(object sender, EventArgs e)
+        {
+
+            if (file_name.Length > 0)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(file_name);
+
+                string json_string = sr.ReadToEnd();
+                sr.Close();
+                System.IO.StreamReader sr2 = new System.IO.StreamReader(file_name2);
+
+                string json_string2 = sr2.ReadToEnd();
+                sr2.Close();
+                list = new JArray();
+                JArray source = JsonConvert.DeserializeObject<JArray>(json_string);
+                JArray source2 = JsonConvert.DeserializeObject<JArray>(json_string2);
+                int i = 0;
+                foreach (var item in source)
+                {
+                    JObject jo = new JObject();
+                    jo["id"] = item["id"];
+                    jo["num"] = source2[i]["num"];
+                    jo["title"] = item["title"];
+                    jo["text"] = item["text"];
+                    jo["type"] = item["type"];
+                    string[] info1 = item["info1"].ToString().Split(new string[] { "<br>" }, StringSplitOptions.None);
+                    jo["overview"] = info1[0];
+                    jo["poetry"] = info1[1].Replace("靈籤之曰：","");
+                    jo["explanation"] = item["info3"];
+                    jo["remark"] = item["info4"];
+
+                    JArray interpretation = new JArray();
+                    String inttext = item["info2"].ToString().Replace("<br>", "　");
+                    List<string> intarray = inttext.Split(new string[] { "　" }, StringSplitOptions.None).ToList();
+                    foreach (var int_item in intarray)
+                    {
+                        string[] int_items = int_item.Split(new string[] { "－" }, StringSplitOptions.None);
+                        JObject int_jo = new JObject();
+                        if (int_items.Length >= 2)
+                        {
+                            int_jo["key"] = int_items[0];
+                            int_jo["value"] = int_items[1];
+                            interpretation.Add(int_jo);
+                        }
+
+                    }
+
+
+                    jo["interpretation"] = interpretation;
+                    ++i;
+
+                    list.Add(jo);
+                }
+
+                
+
+                tb_output.Text = list.ToString();
+            }
+            else
+            {
+                MessageBox.Show("File is not opened.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_open2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file_name2 = openFileDialog1.FileName;
+            }
+            else
+            {
+                file_name2 = "";
+            }
+            tb_file2.Text = file_name2;
+        }
+
+        private void btn_process3_Click(object sender, EventArgs e)
+        {
+            if (file_name.Length > 0)
+            {
+                System.IO.StreamReader sr = new
+                      System.IO.StreamReader(openFileDialog1.FileName);
+                string line = "";
+                int i = 0;
+                list = new JArray();
+                list_csv = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] sl = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (sl.Length > 2)
+                    {
+                        if (sl[0].Length == 2)
+                        {
+                            JObject jo = new JObject();
+                            jo["fan"] = sl[0];
+                            jo["jian"] = sl[1];
+                            list.Add(jo);
+
+                            if (cb_csv.Checked)
+                            {
+                                list_csv += "\"" + jo["fan"] + "\"," + "\"" + jo["jian"] + "\"\r\n";
+                            }
+                        }
+
+                    }
+                    //++i;
+                    //if (i > 10) break;
+                }
+
+                sr.Close();
+
+                if (cb_csv.Checked) tb_output.Text = list_csv;
+                else tb_output.Text = list.ToString();
+            }
+            else
+            {
+                MessageBox.Show("File is not opened.");
+            }
+        }
+
+        private void btn_process4_Click(object sender, EventArgs e)
+        {
+            if (file_name.Length > 0)
+            {
+                System.IO.StreamReader sr = new
+                      System.IO.StreamReader(openFileDialog1.FileName);
+                string line = "";
+                int i = 0;
+                list = new JArray();
+                list_csv = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] sl = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (sl.Length >= 2)
+                    {
+                        string[] sl2 = sl[1].Split('/');
+                        JObject jo = new JObject();
+                        if (sl2.Length > 0 && sl2[0].Length == 2)
+                        {
+                            if (sl2.Length > 1)
+                            {
+                                jo["fan"] = sl2[0];
+                                jo["jian"] = sl2[1];
+                            }
+                            else if (sl2.Length == 1)
+                            {
+                                jo["fan"] = sl2[0];
+                                jo["jian"] = sl2[0];
+                            }
+
+                            
+
+                            if (cb_csv.Checked)
+                            {
+                                list_csv += "\"" + jo["fan"] + "\"," + "\"" + jo["jian"] + "\"\r\n";
+                            }else
+                            {
+                                list.Add(jo);
+                            }
+                        }
+                    }
+                    //++i;
+                    //if (i > 10) break;
+                }
+
+                sr.Close();
+                if (cb_csv.Checked) tb_output.Text = list_csv;
+                else tb_output.Text = list.ToString();
+            }
+            else
+            {
+                MessageBox.Show("File is not opened.");
+            }
+        }
+
+        private void btn_process_save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new
+                   System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.UTF8);
+
+                if (file_name.Length > 0)
+                {
+                    System.IO.StreamReader sr = new
+                          System.IO.StreamReader(openFileDialog1.FileName);
+                    string line = "";
+                    int i = 0;
+                    list = new JArray();
+                    list_csv = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] sl = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (sl.Length >= 2)
+                        {
+                            string[] sl2 = sl[1].Split('/');
+                            JObject jo = new JObject();
+                            if (sl2.Length > 0 && sl2[0].Length == 2)
+                            {
+                                if (sl2.Length > 1)
+                                {
+                                    jo["fan"] = sl2[0];
+                                    jo["jian"] = sl2[1];
+                                }
+                                else if (sl2.Length == 1)
+                                {
+                                    jo["fan"] = sl2[0];
+                                    jo["jian"] = sl2[0];
+                                }
+
+                                if (cb_csv.Checked) sw.WriteLine("\"" + jo["fan"] + "\"," + "\"" + jo["jian"] + "\"");
+                                else sw.WriteLine(jo.ToString());
+                            }
+                        }
+                    }
+
+                    sr.Close();
+                    MessageBox.Show("File saved.");
+                }
+                else
+                {
+                    MessageBox.Show("File is not opened.");
+                }
+
+                sw.Close();
+            }
+        }
+
+        private void btn_process5_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new
+                   System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.UTF8);
+
+                if (file_name.Length > 0)
+                {
+                    System.IO.StreamReader sr = new
+                          System.IO.StreamReader(openFileDialog1.FileName);
+                    string line = "";
+                    int i = 0;
+                    list = new JArray();
+                    list_csv = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        
+                        if (line.Length == 2)
+                        {
+                            if (line[0] != line[1])
+                            {
+                                sw.WriteLine(line);
+                            }
+                        }
+                    }
+
+                    sr.Close();
+                    MessageBox.Show("File saved.");
+                }
+                else
+                {
+                    MessageBox.Show("File is not opened.");
+                }
+
+                sw.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter sw = new
+                   System.IO.StreamWriter(saveFileDialog1.FileName, false, System.Text.Encoding.UTF8);
+
+                if (file_name.Length > 0)
+                {
+                    System.IO.StreamReader sr = new
+                          System.IO.StreamReader(openFileDialog1.FileName);
+                    string line = "";
+                    int i = 0;
+                    list = new JArray();
+                    list_csv = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+
+                        if (line.Length == 2)
+                        {
+                            if (line[0] != line[1])
+                            {
+                                sw.WriteLine(line[0] + "," + line[1]);
+                            }
+                        }
+                    }
+
+                    sr.Close();
+                    MessageBox.Show("File saved.");
+                }
+                else
+                {
+                    MessageBox.Show("File is not opened.");
+                }
+
                 sw.Close();
             }
         }
